d2e080e149e8da6fe5e24b6fa97337a3e98060e4
Renato Garbim <renatobordinigarbim@hotmail.com>
2022-12-22 20:05:01 -0300

    exchange exercises

--- Exchange_Receiver/Exchange_Receiver/Program.cs
+++ Exchange_Receiver/Exchange_Receiver/Program.cs
@@ -12,24 +12,37 @@ namespace Exchange_Receiver
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.ExchangeDeclare(exchange: "logs", type: ExchangeType.Fanout);
-
+                channel.ExchangeDeclare(exchange: "direct_logs",type: "direct");
                 var queueName = channel.QueueDeclare().QueueName;
 
-                channel.QueueBind(queue: queueName,
-                                  exchange: "logs",
-                                  routingKey: "");
+                if (args.Length < 1)
+                {
+                    Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                                            Environment.GetCommandLineArgs()[0]);
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
 
-                Console.WriteLine(" [*] Waiting for logs.");
+                foreach (var severity in args)
+                {
+                    channel.QueueBind(queue: queueName,
+                                      exchange: "direct_logs",
+                                      routingKey: severity);
+                }
+
+                Console.WriteLine(" [*] Waiting for messages.");
 
                 var consumer = new EventingBasicConsumer(channel);
                 consumer.Received += (model, ea) =>
                 {
                     var body = ea.Body.ToArray();
                     var message = Encoding.UTF8.GetString(body);
-                    Console.WriteLine(" [x] {0}", message);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine(" [x] Received '{0}':'{1}'",
+                                      routingKey, message);
                 };
-
                 channel.BasicConsume(queue: queueName,
                                      autoAck: true,
                                      consumer: consumer);
