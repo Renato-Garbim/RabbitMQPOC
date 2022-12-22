c9a909fd18be46f007a36b0dbd1e12becbcb9c00
RenatoGarbim <renatobordinigarbim@hotmail.com>
2022-12-21 21:26:55 -0300

    First Commit

---
+++ Receive/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "task_queue",
+                         durable: true,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [x] Received {0}", message);
+
+        int dots = message.Split('.').Length - 1;
+        Thread.Sleep(dots * 1000);
+
+        // Configuração de Acknowledge do RabbitMQ, informa para a Queu que a msg foi recebida.
+
+        channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+
+    };
+
+    channel.BasicConsume(queue: "task_queue",
+                         autoAck: false,
+                         consumer: consumer);
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
---
+++ Send/Program.cs
@@ -0,0 +1,47 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send;
+class Program
+{
+    private static string GetMessage(string[] args)
+    {
+        return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+    }
+
+    static void Main(string[] args)
+    {
+        var message = "";
+
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+
+            //setando a Queu para durar mais tempo mesmo após uma request
+            channel.QueueDeclare(queue: "task_queue",
+                              durable: true,
+                              exclusive: false,
+                              autoDelete: false,
+                              arguments: null);
+
+            message = GetMessage(args);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            //Setando a msg para nao expirar 
+            var properties = channel.CreateBasicProperties();
+            properties.Persistent = true;
+
+            channel.BasicPublish(exchange: "",
+                                 routingKey: "task_queue",
+                                 basicProperties: null,
+                                 body: body);
+
+            Console.WriteLine(" [x] Sent {0}", message);
+        }
+
+        Console.WriteLine(" Press [enter] to exit.");
+        Console.ReadLine();
+    }
+}
